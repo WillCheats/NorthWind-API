be5f7711f1fdc04e9546c58617b7d81d39225430
Max Roberts <MRoberts@spartaglobal.com>
2022-11-11 10:47:47 +0000

    Changed JSON Format

--- src/main/java/com/example/loki/winners/northwindapi/controller/EmployeeController.java
+++ src/main/java/com/example/loki/winners/northwindapi/controller/EmployeeController.java
@@ -5,7 +5,9 @@ import com.example.loki.winners.northwindapi.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class EmployeeController {
 
     private EmployeeRepository repo;
--- src/main/java/com/example/loki/winners/northwindapi/controller/TerritoryController.java
+++ src/main/java/com/example/loki/winners/northwindapi/controller/TerritoryController.java
@@ -6,14 +6,18 @@ import com.example.loki.winners.northwindapi.repository.EmployeeRepository;
 import com.example.loki.winners.northwindapi.repository.EmployeeterritoryRepository;
 import com.example.loki.winners.northwindapi.repository.TerritoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.loki.winners.northwindapi.entity.Territory;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -32,27 +36,40 @@ public class TerritoryController {
     }
 
     @GetMapping("/territory/all")
-    public List<EntityModel<Territory>> getTerritoryByID() {
-        List<EntityModel<Territory>> models = new ArrayList<>();
+    public List<Map<String, Object>> getTerritoryByID() {
+        List<Map<String, Object>> territoryMaps = new ArrayList<>();
 
         List<Territory> territoryList = territoryRepository.findAll();
         List<Employeeterritory> employeeterritoryRepositoryList = employeeterritoryRepository.findAll();
 
         for (Territory territory : territoryList) {
-            EntityModel<Territory> territoryModel = EntityModel.of(territory);
-            for (Employeeterritory employeeterritory : employeeterritoryRepositoryList) {
-                if (employeeterritory.getTerritoryID().equals(territory)) {
-                    Employee employee = employeeterritory.getEmployeeID();
-                    Link link = linkTo(methodOn(EmployeeController.class)
-                            .getEmployeeById(employee.getId()))
-                            .withRel(employee.getFirstName() + " " + employee.getLastName());
-                    territoryModel.add(link);
-                }
-            }
-            models.add(territoryModel);
+            Map<String, Link> employeeMap = getEmployeeMap(employeeterritoryRepositoryList, territory);
+            territoryMaps.add(getTerritoryJSON(territory, employeeMap));
         }
 
-        return models;
+        return territoryMaps;
+    }
+
+    private static Map<String, Link> getEmployeeMap(List<Employeeterritory> employeeterritoryRepositoryList, Territory territory) {
+        Map<String, Link> employeeMap = new HashMap<>();
+        for (Employeeterritory employeeterritory : employeeterritoryRepositoryList) {
+            if (employeeterritory.getTerritoryID().equals(territory)) {
+                Employee employee = employeeterritory.getEmployeeID();
+                Link link = linkTo(methodOn(EmployeeController.class)
+                        .getEmployeeById(employee.getId()))
+                        .withRel("employee" + employee.getId());
+                employeeMap.put(employee.getFirstName() + " " + employee.getLastName(), link);
+            }
+        }
+        return employeeMap;
+    }
+
+    private Map<String, Object> getTerritoryJSON(Territory territory, Map<String, Link> employeeMap) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", territory.getId());
+        map.put("description", territory.getTerritoryDescription());
+        map.put("employees", employeeMap);
+        return map;
     }
 
 }
