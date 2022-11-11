81c8e597bf48ce07cfc8fa2ab26b4815348090c4
Max Roberts <MRoberts@spartaglobal.com>
2022-11-11 11:28:40 +0000

    Added getTerritoryByID Endpoint

--- src/main/java/com/example/loki/winners/northwindapi/controller/TerritoryController.java
+++ src/main/java/com/example/loki/winners/northwindapi/controller/TerritoryController.java
@@ -2,22 +2,21 @@ package com.example.loki.winners.northwindapi.controller;
 
 import com.example.loki.winners.northwindapi.entity.Employee;
 import com.example.loki.winners.northwindapi.entity.Employeeterritory;
+import com.example.loki.winners.northwindapi.exception.EntityNotFoundException;
 import com.example.loki.winners.northwindapi.repository.EmployeeRepository;
 import com.example.loki.winners.northwindapi.repository.EmployeeterritoryRepository;
 import com.example.loki.winners.northwindapi.repository.TerritoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
-import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.loki.winners.northwindapi.entity.Territory;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -35,6 +34,15 @@ public class TerritoryController {
         this.employeeterritoryRepository = employeeterritoryRepository;
     }
 
+    @GetMapping("/territory/{id}")
+    public Map<String, Object> getTerritoryByID(@PathVariable String id) {
+        Territory territory = territoryRepository.findById(id).orElseThrow(()
+                -> new EntityNotFoundException(404, "Could not find territory with id " + id + "."));
+        return getTerritoryMap(territory,
+                getEmployeeMap(employeeterritoryRepository.findAll(), territory)
+        );
+    }
+
     @GetMapping("/territory/all")
     public List<Map<String, Object>> getTerritoryByID() {
         List<Map<String, Object>> territoryMaps = new ArrayList<>();
@@ -44,10 +52,17 @@ public class TerritoryController {
 
         for (Territory territory : territoryList) {
             Map<String, Link> employeeMap = getEmployeeMap(employeeterritoryRepositoryList, territory);
-            territoryMaps.add(getTerritoryJSON(territory, employeeMap));
+            territoryMaps.add(getTerritoryMap(territory, employeeMap));
         }
 
         return territoryMaps;
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> catchTerritoryNotFoundException(EntityNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.toMap());
     }
 
     private static Map<String, Link> getEmployeeMap(List<Employeeterritory> employeeterritoryRepositoryList, Territory territory) {
@@ -57,20 +72,21 @@ public class TerritoryController {
                 Employee employee = employeeterritory.getEmployeeID();
                 Link link = linkTo(methodOn(EmployeeController.class)
                         .getEmployeeById(employee.getId()))
-                        .withRel("employee" + employee.getId());
+                        .withRel("employee " + employee.getId());
                 employeeMap.put(employee.getFirstName() + " " + employee.getLastName(), link);
             }
         }
         return employeeMap;
     }
 
-    private Map<String, Object> getTerritoryJSON(Territory territory, Map<String, Link> employeeMap) {
+    private Map<String, Object> getTerritoryMap(Territory territory, Map<String, Link> employeeMap) {
         Map<String, Object> map = new HashMap<>();
         map.put("id", territory.getId());
         map.put("description", territory.getTerritoryDescription());
         map.put("employees", employeeMap);
         return map;
     }
+
 
 }
 
